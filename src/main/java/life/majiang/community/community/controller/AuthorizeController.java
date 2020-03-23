b2f8f943adb706d631c5a24233070a068a7604cf
Tommy <1215218509@qq.com>
2020-03-23 10:20:24 +0800

    '添加到配置文件'

--- src/main/java/life/majiang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -3,29 +3,40 @@ package life.majiang.community.community.controller;
 import life.majiang.community.community.dto.AccessTokenDTO;
 import life.majiang.community.community.dto.GithubUser;
 import life.majiang.community.community.provider.GithubProvider;
-import org.json.JSONArray;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.url}")
+    private String redirectUri;
+
 
     @Autowired
     private GithubProvider githubProvider;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name="state") String state) throws JSONException {
+                           @RequestParam(name="state") String state, HttpServletRequest request) throws JSONException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_id("3e677354ee2bb4c4e414");
-        accessTokenDTO.setClient_secret("http://127.0.0.1:8887/callback");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_secret("1928d7ad34206561015fa8c0d547425799827b87");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         System.out.println("so:"+accessTokenDTO.toString());
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         System.out.println("这是"+accessToken);
@@ -33,12 +44,19 @@ public class AuthorizeController {
 
         GithubUser githubUser= new GithubUser();
 
+
         JSONObject jsonObject = new JSONObject(user);
 
         JSONObject personObject = jsonObject.getJSONObject("plan");
         String result = jsonObject.getString("login");
 
         String name = personObject.getString("name");
+        githubUser.setName(name);
+
+        if(name!=null){
+            request.getSession();
+        }
+
         System.out.println(result);
         //int id = jsonObject.getInt("id");
         //String login = jsonObject.getString("login");
